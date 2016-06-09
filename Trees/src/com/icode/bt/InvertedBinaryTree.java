2f9c603eb9ff8289b83a192027efd117bc98025d
Sugandha Naolekar <sugandha.n87@gmail.com>
2016-06-09 14:46:11 -0700

    Added InvertedBinaryTree.java

---
+++ Trees/src/com/icode/bt/InvertedBinaryTree.java
@@ -0,0 +1,16 @@
+//Invert a binary tree
+//OR convert a binary tree into it's mirror image
+
+/*
+ * @Author Sugandha Naolekar
+ */
+package com.icode.bt;
+
+public class InvertedBinaryTree {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
