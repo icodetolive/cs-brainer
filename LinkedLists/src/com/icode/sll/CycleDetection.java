b3c47f65354d0b4bbe12b9e2e859b06c706a8f81
Sugandha Naolekar <sugandha.n87@gmail.com>
2016-10-23 20:12:23 -0700

    feat: Add fetchStartingNodeOfCycle()

--- LinkedLists/src/com/icode/sll/CycleDetection.java
+++ LinkedLists/src/com/icode/sll/CycleDetection.java
@@ -1,5 +1,19 @@
 package com.icode.sll;
-
+/*
+ * @Author Sugandha
+ * 
+ * Following functions included:
+ * 
+ * 1. Detect if a singly linked list has a cycle
+ * 2. If yes, fetch the entrance/starting node of the cycle
+ * 3. If yes, fetch the length of the cycle
+ * 4. If yes, remove the loop from the cycle
+ * 
+ * Reference(s): 
+ * Detect cycle: http://buttercola.blogspot.com/2014/08/leetcode-linked-list-cycle.html
+ * Fetch entrance node of cycle and length of list: http://buttercola.blogspot.com/2014/08/leetcode-linked-list-cycle-ii.html
+ * Remove loop: http://www.geeksforgeeks.org/detect-and-remove-loop-in-a-linked-list/
+ */
 public class CycleDetection {
 
 	public static void main(String[] args) {
@@ -12,10 +26,17 @@ public class CycleDetection {
 		SLLNode head = list.getHead();
 		
 		//create loop for testing
-		list.getHead().next.next.next.next.next = head;
+		list.getHead().next.next.next.next.next = head.next;
 
-		boolean result = hasCycle(list.getHead());
-		System.out.println("Linked list has cycle? ::"+result);
+//		boolean result = hasCycle(head);
+//		System.out.println("Linked list has cycle? ::"+result);
+		SLLNode result = fetchStartingNodeOfCycle(head);
+		if(result != null) {
+			System.out.println("The cycle starts at: "+result.data);
+		}
+		else {
+			System.out.println("The list doesn't contain cycle!");
+		}
 	}
 
 	
@@ -39,4 +60,43 @@ public class CycleDetection {
 		}
 		return false;
 	}
+	
+	public static SLLNode fetchStartingNodeOfCycle(SLLNode head) {
+		  if(head == null) {
+	            return null;
+	        }
+	        
+	        SLLNode temp = hasCycleCustom(head);
+	        
+	        //check if there is a cycle
+	        if(temp == null || temp.next == null) {
+	            return null;
+	        }
+	        
+	        //fetch the entrance node of the cycle
+	        SLLNode slow = head;
+	        while(slow != temp) {
+	            slow = slow.next;
+	            temp = temp.next;
+	        }
+	        
+	        return slow;
+	    }
+	
+	private static SLLNode hasCycleCustom(SLLNode head) {
+		SLLNode slow = head;
+		SLLNode fast = head;
+		
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			
+			if(slow == fast) {
+				break;
+			}
+		}
+		
+		return fast;
+	}
+	  
 }
