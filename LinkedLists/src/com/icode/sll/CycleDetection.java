f52845c7017114d27b8539511d07e9d0e9978dac
Sugandha Naolekar <sugandha.n87@gmail.com>
2016-10-30 17:03:41 -0700

    fix: Calculate length of cycle

--- LinkedLists/src/com/icode/sll/CycleDetection.java
+++ LinkedLists/src/com/icode/sll/CycleDetection.java
@@ -11,7 +11,7 @@ package com.icode.sll;
  * 
  * Reference(s): 
  * Detect cycle: http://buttercola.blogspot.com/2014/08/leetcode-linked-list-cycle.html
- * Fetch entrance node of cycle and length of list: http://buttercola.blogspot.com/2014/08/leetcode-linked-list-cycle-ii.html
+ * Fetch starting node of cycle and length of cycle: http://buttercola.blogspot.com/2014/08/leetcode-linked-list-cycle-ii.html
  * Remove loop: http://www.geeksforgeeks.org/detect-and-remove-loop-in-a-linked-list/
  */
 public class CycleDetection {
@@ -26,11 +26,12 @@ public class CycleDetection {
 		SLLNode head = list.getHead();
 		
 		//create loop for testing
-		list.getHead().next.next.next.next.next = head.next;
+//		1-2-3-4-5-2
+//		list.getHead().next.next.next.next.next = head.next;
 
 //		boolean result = hasCycle(head);
 //		System.out.println("Linked list has cycle? ::"+result);
-//		SLLNode result = fetchStartingNodeOfCycle(head);
+//		SLLNode result = fetchStartingNodeOfCycle(head); //2
 //		if(result != null) {
 //			System.out.println("The cycle starts at: "+result.data);
 //		}
@@ -38,8 +39,12 @@ public class CycleDetection {
 //			System.out.println("The list doesn't contain cycle!");
 //		}
 		
-		int result = calculateLength(head);
-		System.out.println();
+		int result = calculateLengthOfCycle(head); //4
+		if(result != -1) {
+			System.out.println("No. of nodes in cycle: "+result);
+		}else {
+			System.out.println("The list does not have a loop/cycle");
+		}
 	}
 
 	
@@ -112,12 +117,11 @@ public class CycleDetection {
 	/*
 	 * Algorithm:
 	 * 1. Find the collision point 
-	 * 2. From the collision point in the cycle, use two pointers - 
-	 *    One moves one step at a time while the other moves two at a time. 
+	 * 2. From the collision point in the cycle, move a pointer one step at a time while the other stands still. 
 	 * 3. Until they meet again, the number of steps is the length of the cycle. 
 	 */
 	
-	public static int calculateLength(SLLNode head) {
+	public static int calculateLengthOfCycle(SLLNode head) {
 		
 		if(head == null) {
 			return -1;
@@ -132,19 +136,13 @@ public class CycleDetection {
 //        
 		SLLNode fast = temp;
 		SLLNode slow = fast;
-		int counter = 0;
+		int counter = 1;
 		
-		slow = slow.next;
-		fast = fast.next.next;
-		counter++;	
-		
-		while(slow != fast) {
-			slow = slow.next;
-			fast = fast.next.next;
+		while(fast.next != slow) {
+			fast = fast.next;
 			counter++;
 		}
 		
-		System.out.println("--"+counter);
 		return counter;
 	}
 	  
